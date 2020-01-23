62505853d6f0772765aa1efb610a25aca8a0f912
McLaughlin <mcmclaughlin@labs.neit.edu>
2020-01-23 14:26:16 -0500

    assignment 1 list of movies and 2 other pages

---
+++ app.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname +'/public'));
+
+app.get('/', function(req, res){
+    res.sendFile('index.html')
+});
+
+app.listen(3000, function(){
+    console.log("The server is started on port 3000")
+})
